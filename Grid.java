ea452a3add5ba3db28f010b574b5e616344ff17f
riannktang <rtang@wellesley.edu>
2017-04-26 21:42:42 -0400

    Add files via upload
    Finished and tested

--- Grid.java
+++ Grid.java
@@ -1,5 +1,3 @@
-// Need to account for ships which overlap
-
 import java.util.*;
 
 public class Grid {
@@ -23,20 +21,42 @@ public class Grid {
     // Example: Cruiser (length 3) horizontal from A1 to A3
     char startRow = ship.getStartCoord().charAt(0);
     char startCol = ship.getStartCoord().charAt(1);
+    boolean alreadyShip = false;
     
     if(ship.getOrientation().equals("Vertical")) {
+      // First for loop to ensure that ship does not already exist in locations
       for(int i = 0; i < ship.getLength(); i++) {
-        String loc = (char)(startRow + i) + Character.toString(startCol);
-        locations.put(loc, true); 
+    	  String loc = (char)(startRow + i) + Character.toString(startCol);
+          if (locations.get(loc).equals(true)) {
+        	 System.out.println("There is already a ship in this location!"); 
+        	 alreadyShip = true;
+        	 break;
+          }
+      }
+      for(int i = 0; i < ship.getLength(); i++) {
+    	  if (!alreadyShip) {
+    	  String loc = (char)(startRow + i) + Character.toString(startCol);
+    	  locations.put(loc, true);
+    	  }
       }
     }
     
     else {
-      for(int j = 0; j < ship.getLength(); j++) {
-        String loc = Character.toString(startRow) + (char)(startCol + j);
-        locations.put(loc, true); 
+        for(int i = 0; i < ship.getLength(); i++) {
+        	String loc = Character.toString(startRow) + (char)(startCol + i);
+            if (locations.get(loc).equals(true)) {
+          	 System.out.println("There is already a ship in this location!"); 
+          	 alreadyShip = true;
+          	 break;
+            }
+        }
+        for(int i = 0; i < ship.getLength(); i++) {
+      	  if (!alreadyShip) {
+      	  String loc = Character.toString(startRow) + (char)(startCol + i);
+      	  locations.put(loc, true);
+      	  }
+        }
       }
-    }
   }
   
   public boolean markMissile(String location) {
@@ -55,10 +75,10 @@ public class Grid {
 	Grid grid = new Grid();
 	
     Ship ship = new Ship("Destroyer", "Vertical", "C4");
-    Ship ship2 = new Ship("Submarine", "Horizontal", "A5");
+    Ship ship2 = new Ship("Submarine", "Horizontal", "C3");
 
-    /*grid.addShip(ship);
-    System.out.println("Checking if C4 has a ship: " + grid.locations.get("C4"));
+    grid.addShip(ship);
+    /*System.out.println("Checking if C4 has a ship: " + grid.locations.get("C4"));
     System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
     grid.markMissile("C4");
     grid.markMissile("A4");
@@ -66,11 +86,11 @@ public class Grid {
     System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));*/
     
     grid.addShip(ship2);
-    System.out.println("Checking if A5 has a ship: " + grid.locations.get("A5"));
-    System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
+    System.out.println("Checking if C3 has a ship: " + grid.locations.get("C3"));
+    /*System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
     grid.markMissile("A5");
     grid.markMissile("A4");
     System.out.println("Checking if A5 has been hit: " + grid.gridState.get("A5"));
-    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));
+    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));*/
   }
 }
