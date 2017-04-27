4b2a26237560fe9e922e0135febc5dcfcd7dfa4d
riannktang <rtang@wellesley.edu>
2017-04-27 13:22:26 -0400

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class Grid {
-  private Hashtable<String, String> gridState;
-  private Hashtable<String, Boolean> locations; 
+  private Hashtable<String, String> targetGrid;
+  private Hashtable<String, Boolean> oceanGrid; 
   
   public Grid() {
-    gridState = new Hashtable<String, String>();
-    locations = new Hashtable<String, Boolean>();
+    targetGrid = new Hashtable<String, String>();
+    oceanGrid = new Hashtable<String, Boolean>();
     
     for (int i = 65; i < 75; i ++) {
       for (int j = 1; j <11; j++) {
-        gridState.put((char)(i) + Integer.toString(j), "Empty");
-        locations.put((char)(i) + Integer.toString(j), false);
+    	targetGrid.put((char)(i) + Integer.toString(j), "Empty");
+    	oceanGrid.put((char)(i) + Integer.toString(j), false);
       }
     }
   }
@@ -27,7 +27,7 @@ public class Grid {
       // First for loop to ensure that ship does not already exist in locations
       for(int i = 0; i < ship.getLength(); i++) {
     	  String loc = (char)(startRow + i) + Character.toString(startCol);
-          if (locations.get(loc).equals(true)) {
+          if (oceanGrid.get(loc).equals(true)) {
         	 System.out.println("There is already a ship in this location!"); 
         	 alreadyShip = true;
         	 break;
@@ -36,7 +36,7 @@ public class Grid {
       for(int i = 0; i < ship.getLength(); i++) {
     	  if (!alreadyShip) {
     	  String loc = (char)(startRow + i) + Character.toString(startCol);
-    	  locations.put(loc, true);
+    	  oceanGrid.put(loc, true);
     	  }
       }
     }
@@ -44,7 +44,7 @@ public class Grid {
     else {
         for(int i = 0; i < ship.getLength(); i++) {
         	String loc = Character.toString(startRow) + (char)(startCol + i);
-            if (locations.get(loc).equals(true)) {
+            if (oceanGrid.get(loc).equals(true)) {
           	 System.out.println("There is already a ship in this location!"); 
           	 alreadyShip = true;
           	 break;
@@ -53,23 +53,19 @@ public class Grid {
         for(int i = 0; i < ship.getLength(); i++) {
       	  if (!alreadyShip) {
       	  String loc = Character.toString(startRow) + (char)(startCol + i);
-      	  locations.put(loc, true);
+      	  oceanGrid.put(loc, true);
       	  }
         }
       }
   }
   
-  public void markMissile(String location) {
-    if (gridState.containsKey(location)) {
-	    if(locations.get(location) == true) {
-	      gridState.put(location, "HIT");
-	    }
-	    else {
-	      gridState.put(location, "MISS");
-	    }
-    }
-    System.out.println("The chosen location is invalid.");
+  public void markHitMissile(String location) {
+	 targetGrid.put(location, "HIT");
   }
+  
+  public void markMissMissile(String location) {
+	 targetGrid.put(location, "MISS");
+	  }
   
   public static void main(String[] args) {
 	Grid grid = new Grid();
@@ -78,20 +74,20 @@ public class Grid {
     Ship ship2 = new Ship("Submarine", "Horizontal", "C3");
 
     grid.addShip(ship);
-    /*System.out.println("Checking if C4 has a ship: " + grid.locations.get("C4"));
-    System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
+    /*System.out.println("Checking if C4 has a ship: " + grid.oceanGrid.get("C4"));
+    System.out.println("Checking if D4 has a ship: " + grid.oceanGrid.get("D4"));
     grid.markMissile("C4");
     grid.markMissile("A4");
-    System.out.println("Checking if C4 has been hit: " + grid.gridState.get("C4"));
-    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));*/
+    System.out.println("Checking if C4 has been hit: " + grid.targetGrid.get("C4"));
+    System.out.println("Checking if A4 has been hit: " + grid.targetGrid.get("A4"));*/
     
     grid.addShip(ship2);
-    System.out.println("Checking if C3 has a ship: " + grid.locations.get("C3"));
-    grid.markMissile("Z5");
-    /*System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
+    System.out.println("Checking if C3 has a ship: " + grid.oceanGrid.get("C3"));
+    //grid.markMissile("Z5");
+    /*System.out.println("Checking if D4 has a ship: " + grid.oceanGrid.get("D4"));
     grid.markMissile("A5");
     grid.markMissile("A4");
-    System.out.println("Checking if A5 has been hit: " + grid.gridState.get("A5"));
-    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));*/
+    System.out.println("Checking if A5 has been hit: " + grid.targetGrid.get("A5"));
+    System.out.println("Checking if A4 has been hit: " + grid.targetGrid.get("A4"));*/
   }
 }
