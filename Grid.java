32afb076f5042a70ac722f74585b08b2574ccde2
riannktang <rtang@wellesley.edu>
2017-04-26 21:17:40 -0400

    Add files via upload
    Grid works now except in the case where two ships overlap, then kabam, shit goes down

--- Grid.java
+++ Grid.java
@@ -1,4 +1,5 @@
-//jakldfjadskl
+// Need to account for ships which overlap
+
 import java.util.*;
 
 public class Grid {
@@ -15,38 +16,31 @@ public class Grid {
         locations.put((char)(i) + Integer.toString(j), false);
       }
     }
-    /*
-    ships.add(new Ship("Destroyer", "vertical", "A1"));
-    ships.add(new Ship("Submarine", "vertical", "A1"));
-    ships.add(new Ship("Cruiser", "vertical", "A1"));
-    ships.add(new Ship("Battleship", "vertical", "A1"));
-    ships.add(new Ship("Carrier", "vertical", "A1"));
-    */
   }
   
   public void addShip(Ship ship) {
     // Example: Cruiser (length 3) vertical from A1 to C1 
     // Example: Cruiser (length 3) horizontal from A1 to A3
-    int startRow = (int) ship.getStartCoord().charAt(0);
+    char startRow = ship.getStartCoord().charAt(0);
     char startCol = ship.getStartCoord().charAt(1);
     
     if(ship.getOrientation().equals("Vertical")) {
       for(int i = 0; i < ship.getLength(); i++) {
-        String loc = Integer.toString(startRow + i) + startCol;
+        String loc = (char)(startRow + i) + Character.toString(startCol);
         locations.put(loc, true); 
       }
     }
     
     else {
       for(int j = 0; j < ship.getLength(); j++) {
-        String loc = startRow + Integer.toString((int) (startCol + j));
+        String loc = Character.toString(startRow) + (char)(startCol + j);
         locations.put(loc, true); 
       }
     }
   }
   
   public boolean markMissile(String location) {
-    // check if shot is in bounds
+    // Still need method for if missile is shot at an out of bounds location
     if(locations.get(location) == true) {
       gridState.put(location, "HIT");
       return true;
@@ -57,53 +51,26 @@ public class Grid {
     }
   }
   
-  
-  /*
-  public void placeDestroyer(String hole1, String hole2) {
-    shipLocations.get(0).add(0, hole1);
-    shipLocations.get(0).add(1, hole2);
-  }
-  
-  public void placeSubmarine (String hole1, String hole2, String hole3) {
-    shipLocations.get(1).add(0, hole1);
-    shipLocations.get(1).add(1, hole2);
-    shipLocations.get(1).add(2, hole3);
-  }
-  
-  public void placeDestoryer (String hole1, String hole2, String hole3) {
-    shipLocations.get(2).add(0, hole1);
-    shipLocations.get(2).add(1, hole2);
-    shipLocations.get(2).add(2, hole3);
-  }
-  
-  public void placeBattleship (String hole1, String hole2, String hole3, String hole4) {
-    shipLocations.get(3).add(0, hole1);
-    shipLocations.get(3).add(1, hole2);
-    shipLocations.get(3).add(2, hole3);
-    shipLocations.get(3).add(3, hole4);
-  }
-  
-  public void placeCarrier (String hole1, String hole2, String hole3, String hole4, String hole5) {
-    shipLocations.get(4).add(0, hole1);
-    shipLocations.get(4).add(1, hole2);
-    shipLocations.get(4).add(2, hole3);
-    shipLocations.get(4).add(3, hole4);
-    shipLocations.get(4).add(4, hole5);
-  }
-  */
   public static void main(String[] args) {
+	Grid grid = new Grid();
+	
     Ship ship = new Ship("Destroyer", "Vertical", "C4");
-    char startRow = ship.getStartCoord().charAt(0);
-    char startCol = ship.getStartCoord().charAt(1);
+    Ship ship2 = new Ship("Submarine", "Horizontal", "A5");
+
+    /*grid.addShip(ship);
+    System.out.println("Checking if C4 has a ship: " + grid.locations.get("C4"));
+    System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
+    grid.markMissile("C4");
+    grid.markMissile("A4");
+    System.out.println("Checking if C4 has been hit: " + grid.gridState.get("C4"));
+    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));*/
     
-    System.out.println((int) startRow);
-    System.out.println(startCol);
-    /*
-    Grid grid = new Grid();
-    System.out.println(grid.grid.toString());
-    grid.markMissile("A1");
-    System.out.println(grid.grid.get("A1"));
-    */
-    
+    grid.addShip(ship2);
+    System.out.println("Checking if A5 has a ship: " + grid.locations.get("A5"));
+    System.out.println("Checking if D4 has a ship: " + grid.locations.get("D4"));
+    grid.markMissile("A5");
+    grid.markMissile("A4");
+    System.out.println("Checking if A5 has been hit: " + grid.gridState.get("A5"));
+    System.out.println("Checking if A4 has been hit: " + grid.gridState.get("A4"));
   }
 }
