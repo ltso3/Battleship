2620e5d6f806d0ffbdb225f8476e2c5926a597cd
riannktang <rtang@wellesley.edu>
2017-05-02 15:31:13 -0400

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -3,10 +3,16 @@ import java.util.*;
 public class Grid {
   private Hashtable<String, String> targetGrid;
   private Hashtable<String, String> oceanGrid; 
+  private Vector<LinkedList<String>> ships;
+  private static final int NUM_SHIPS = 5;
   
   public Grid() {
     targetGrid = new Hashtable<String, String>();
     oceanGrid = new Hashtable<String, String>();
+    ships = new Vector<LinkedList<String>>();
+    
+    for (int i = 0; i < NUM_SHIPS; i++) // Fill ships with empty lists
+    	ships.add(new LinkedList<String>());
     
     for (int i = 65; i < 75; i ++) {
       for (int j = 1; j <11; j++) {
@@ -34,15 +40,22 @@ public class Grid {
           }
       }
       // Places ship in each space it fills
-      for(int i = 0; i < ship.getLength(); i++) {
-    	  if (!alreadyShip) {
-    	  String loc = (char)(startRow + i) + Character.toString(startCol);
-    	  oceanGrid.put(loc, "ship");
-    	  }
+      for (int j = 0; j < ships.size(); j++) {
+		  if (ships.get(j).isEmpty()) {
+		      for(int i = 0; i < ship.getLength(); i++) {
+		    	  if (!alreadyShip) {
+			    	  String loc = (char)(startRow + i) + Character.toString(startCol);
+			    	  oceanGrid.put(loc, "ship");
+			    	  ships.get(j).add(loc);
+		    	  }
+		      }
+		      j = ships.size();
+		  } 
       }
     }
-    
+
     else {
+    	// First for loop to ensure that ship does not already exist in locations
         for(int i = 0; i < ship.getLength(); i++) {
         	String loc = Character.toString(startRow) + (char)(startCol + i);
             if (oceanGrid.get(loc).equals("ship")) {
@@ -51,11 +64,17 @@ public class Grid {
           	 break;
             }
         }
-        for(int i = 0; i < ship.getLength(); i++) {
-      	  if (!alreadyShip) {
-      	  String loc = Character.toString(startRow) + (char)(startCol + i);
-      	  oceanGrid.put(loc, "ship");
-      	  }
+        for (int j = 0; j < ships.size(); j++) {
+  		  if (ships.get(j).isEmpty()) {
+  		      for(int i = 0; i < ship.getLength(); i++) {
+  		    	  if (!alreadyShip) {
+  		    		String loc = Character.toString(startRow) + (char)(startCol + i);
+  			    	  oceanGrid.put(loc, "ship");
+  			    	  ships.get(j).add(loc);
+  		    	  }
+  		      }
+  		      j = ships.size();
+  		  } 
         }
       }
   }
@@ -84,27 +103,34 @@ public class Grid {
 	  return oceanGrid;
   }
   
+  public Vector<LinkedList<String>> getShips() {
+	  return ships;
+  }
+  
   public static void main(String[] args) {
 	Grid grid = new Grid();
 	
     Ship ship = new Ship("Destroyer", "Vertical", "C4");
     Ship ship2 = new Ship("Submarine", "Horizontal", "C3");
+    Ship ship3 = new Ship("Battleship", "Vertical", "C3");
+    Ship ship4 = new Ship("Cruiser", "Horizantal", "C2");
 
     grid.addShip(ship);
-    /*System.out.println("Checking if C4 has a ship: " + grid.oceanGrid.get("C4"));
-    System.out.println("Checking if D4 has a ship: " + grid.oceanGrid.get("D4"));
-    grid.markMissile("C4");
-    grid.markMissile("A4");
-    System.out.println("Checking if C4 has been hit: " + grid.targetGrid.get("C4"));
-    System.out.println("Checking if A4 has been hit: " + grid.targetGrid.get("A4"));*/
+    System.out.println("Checking if C4 has a ship (ship): " + grid.oceanGrid.get("C4"));
+    System.out.println("Checking if D4 has a ship (ship): " + grid.oceanGrid.get("D4"));
+    grid.markOceanGrid("C4");
+    System.out.println("Checking if C4 has been hit (HIT): " + grid.oceanGrid.get("C4"));
+    System.out.println("Checking if A4 has been targeted (Empty): " + grid.targetGrid.get("A4"));
     
     grid.addShip(ship2);
-    System.out.println("Checking if C3 has a ship: " + grid.oceanGrid.get("C3"));
-    //grid.markMissile("Z5");
-    /*System.out.println("Checking if D4 has a ship: " + grid.oceanGrid.get("D4"));
-    grid.markMissile("A5");
-    grid.markMissile("A4");
-    System.out.println("Checking if A5 has been hit: " + grid.targetGrid.get("A5"));
-    System.out.println("Checking if A4 has been hit: " + grid.targetGrid.get("A4"));*/
+    System.out.println("Checking if C3 has a ship (ship): " + grid.oceanGrid.get("C3"));
+    System.out.println("Checking if D4 has a ship (ship): " + grid.oceanGrid.get("D4"));
+    System.out.println("Checking if A5 has been hit (Empty): " + grid.targetGrid.get("A5"));
+    System.out.println("Checking if A4 has been hit (Empty): " + grid.targetGrid.get("A4"));
+    
+    grid.addShip(ship3);
+    grid.addShip(ship4);
+    
+    System.out.println(grid.getShips());
   }
 }
