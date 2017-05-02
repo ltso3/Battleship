94f32abd61f7ad8e653511ca49a99e2f0f5c14e7
ltso3 <ltso@wellesley.edu>
2017-05-02 19:38:49 -0400

    Add files via upload
    Sound!

--- WelcomePanel.java
+++ WelcomePanel.java
@@ -9,12 +9,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-import java.applet.AudioClip;
-import java.net.URL;
+import javax.sound.sampled.*;
 
 public class WelcomePanel extends JPanel {
   private JButton start;
-  private AudioClip song;
+  AudioFormat audioFormat;
+  AudioInputStream audioInputStream;
+  SourceDataLine sourceDataLine;
+  boolean stopPlayback = false;
   
   public WelcomePanel() {
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); // Sets panel layout to BoxLayout vertically
@@ -27,27 +29,77 @@ public class WelcomePanel extends JPanel {
     start = new JButton("Start");
     start.setPreferredSize(new Dimension(100,50));
     start.setSize(100, 100);
+    start.setEnabled(true);
     start.addActionListener(new ButtonListener());
     button.add(start);
     add(button);
-    
-    URL url1 = null; 
-    
-    try {
-      URL u1 = new URL ("file", "/Users/christinetso/Desktop/CS230/Final%20Project/", "GameOfThrones.wav");
-      System.out.println(u1);
-      song = JApplet.newAudioClip(u1);
-    }
-    catch(Exception e) {
-      System.out.println(e);
-    }
   }
-
+  
   private class ButtonListener implements ActionListener {
     public void actionPerformed (ActionEvent event) {
-      if(event.getSource() == start)
-        song.play();
-      System.out.println(song);
+      start.setEnabled(false);
+      playAudio();
     }
   }
+  
+  private void playAudio() {
+    try {
+      File soundFile = new File("GameOfThrones.wav");
+      audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+      audioFormat = audioInputStream.getFormat();
+      System.out.println(audioFormat);
+      
+      DataLine.Info dataLineInfo =  new DataLine.Info(SourceDataLine.class, audioFormat);
+      
+      sourceDataLine = (SourceDataLine)AudioSystem.getLine(dataLineInfo);
+      
+      //Create a thread to play back the data and start it running.  It will run until the
+      // end of file, or the Stop button is clicked, whichever occurs first.
+      // Because of the data buffers involved there will normally be a delay between
+      // the click on the Stop button and the actual termination of playback.
+      new PlayThread().start();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.exit(0);
+    }//end catch
+  }//end playAudio
+  
+
+//Inner class to play back the data from the
+s// audio file.
+  class PlayThread extends Thread{
+    byte tempBuffer[] = new byte[10000];
+    
+    public void run(){
+      try{
+        sourceDataLine.open(audioFormat);
+        sourceDataLine.start();
+        
+        int cnt;
+        //Keep looping until the input read method returns -1 for empty stream or the
+        // user clicks the Stop button causing stopPlayback to switch from false to
+        // true.
+        while((cnt = audioInputStream.read(tempBuffer,0,tempBuffer.length)) != -1 && stopPlayback == false){
+          if(cnt > 0){
+            //Write data to the internal buffer of
+            // the data line where it will be
+            // delivered to the speaker.
+            sourceDataLine.write(tempBuffer, 0, cnt);
+          }//end if
+        }//end while
+        //Block and wait for internal buffer of the
+        // data line to empty.
+        sourceDataLine.drain();
+        sourceDataLine.close();
+        
+        //Prepare to playback another file
+        start.setEnabled(true);
+        stopPlayback = false;
+      }catch (Exception e) {
+        e.printStackTrace();
+        System.exit(0);
+      }//end catch
+    }//end run
+  }//end inner class PlayThread
 }
