a40c3f83450d28950c27618eb9402e97e31f08b2
riannktang <rtang@wellesley.edu>
2017-05-03 11:02:48 -0400

    Add files via upload

--- Grid.java
+++ Grid.java
@@ -3,17 +3,21 @@ import java.util.*;
 public class Grid {
   private Hashtable<String, String> targetGrid;
   private Hashtable<String, String> oceanGrid; 
+  private Vector<Ship> shipTypes;
   private Vector<LinkedList<String>> ships;
   private static final int NUM_SHIPS = 5;
   
   public Grid() {
+	// Initialize instance variables
     targetGrid = new Hashtable<String, String>();
     oceanGrid = new Hashtable<String, String>();
+    shipTypes = new Vector<Ship>();
     ships = new Vector<LinkedList<String>>();
     
     for (int i = 0; i < NUM_SHIPS; i++) // Fill ships with empty lists
     	ships.add(new LinkedList<String>());
     
+    // Fill hashtables with keys
     for (int i = 65; i < 75; i ++) {
       for (int j = 1; j <11; j++) {
     	targetGrid.put((char)(i) + Integer.toString(j), "Empty");
@@ -21,7 +25,10 @@ public class Grid {
       }
     }
   }
-  
+  /**
+   * Based on the starting point and the orientation of the ship will place the ship on the oceanGrid
+   * @param ship to be placed
+   */
   public void addShip(Ship ship) {
     // Example: Cruiser (length 3) vertical from A1 to C1 
     // Example: Cruiser (length 3) horizontal from A1 to A3
@@ -68,7 +75,7 @@ public class Grid {
   		  if (ships.get(j).isEmpty()) {
   		      for(int i = 0; i < ship.getLength(); i++) {
   		    	  if (!alreadyShip) {
-  		    		String loc = Character.toString(startRow) + (char)(startCol + i);
+  		    		  String loc = Character.toString(startRow) + (char)(startCol + i);
   			    	  oceanGrid.put(loc, "ship");
   			    	  ships.get(j).add(loc);
   		    	  }
@@ -77,9 +84,23 @@ public class Grid {
   		  } 
         }
       }
+    
+    if (!alreadyShip)
+        shipTypes.add(ship);
+  }
+  /**
+   * Alows the user to check if a missile has been already shot to a certain location
+   * @param loc String denoting location to be checked
+   * @return true if a missile has already been shot to location, false otherwise
+   */
+  public boolean alreadyShotMissile(String loc) {
+	  return (targetGrid.equals("HIT") || targetGrid.equals("MISS"));
   }
   
-  // Marking places the player has hit
+ /**
+  * Marks places on the grid where the player has hit
+  * @param location String denoting location to hit
+  */
   public void markTargetGrid(String location) {
 	  if (!oceanGrid.get(location).equals("ship"))
 		  targetGrid.put(location, "MISS");
@@ -87,7 +108,10 @@ public class Grid {
 		  targetGrid.put(location, "HIT");
   }
   
-  // Marking places the player has been hit
+  /**
+   * Makes places on the grid where the player has fired
+   * @param location String denoting location to hit
+   */
   public void markOceanGrid(String location){
 	  if (!oceanGrid.get(location).equals("ship"))
 		  oceanGrid.put(location, "MISS");
@@ -95,16 +119,59 @@ public class Grid {
 		  oceanGrid.put(location, "HIT");
   }
   
+  /**
+   * Getter method for targetGrid
+   * @return Hashtable<String, String> targetGrid
+   */
   public Hashtable<String, String> getTargetGrid() {
 	  return targetGrid;
   }
   
+  /**
+   * Getter method for oceanGrid
+   * @return Hashtable<String, String> oceanGrid
+   */
   public Hashtable<String, String> getOceanGrid() {
 	  return oceanGrid;
   }
   
+  /**
+   * Getter method for ships
+   * @return Vector<LinkedList<String>> ships
+   */
   public Vector<LinkedList<String>> getShips() {
 	  return ships;
+  }
+  
+  /**
+   * Getter method for shipTypes
+   * @return Vector<Ship> ships
+   */
+  public Vector<Ship> getShipTypes() {
+	  return shipTypes;
+  }
+  
+  /**
+   * Returns the index of a given Ship object
+   * @param Ship ship which user wants the index of
+   * @return int index of ship in shipTypes
+   */
+  public int indexOfShip(Ship ship) {
+	  return shipTypes.indexOf(ship);
+  }
+  
+  /**
+   * Method which tells user what ship is stored at a certain location. Assumes user only inputs a location in which a ship exists.
+   * @param loc String denoting which location user would like to search for a ship for
+   * @return Ship ship at input location
+   */
+  public Ship shipAtLocation(String loc) {
+	  int shipIndex = 0;
+	  for (int i = 0; i < ships.size(); i++)
+		  for (String location: ships.get(i))
+			  if (location.equals(loc))
+				  shipIndex = i;
+	  return shipTypes.get(shipIndex);
   }
   
   public static void main(String[] args) {
@@ -128,9 +195,12 @@ public class Grid {
     System.out.println("Checking if A5 has been hit (Empty): " + grid.targetGrid.get("A5"));
     System.out.println("Checking if A4 has been hit (Empty): " + grid.targetGrid.get("A4"));
     
+    System.out.println("Checking ship at location C4 (Submarine): " + grid.shipAtLocation("C4").getName());
+    
     grid.addShip(ship3);
     grid.addShip(ship4);
     
     System.out.println(grid.getShips());
+    System.out.println(grid.getShipTypes());
   }
 }
--- PlayBattleship.java
+++ PlayBattleship.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PlayBattleship {
 	private Grid pGrid;
 	private Grid cGrid;
@@ -5,23 +7,73 @@ public class PlayBattleship {
 	private String cName;
 	
 	public PlayBattleship(String pName, String cName) {
+		// Initialize instance variables
 		pGrid = new Grid();
 		cGrid = new Grid();
 		this.pName = pName;
 		this.cName = cName;
 	}
 	
-	// Fires missile, marks player one's targetGrid and the other player two's oceanGrid
+	/**
+	 * Fires missile, marks player one's targetGrid and the other player two's oceanGrid
+	 * @param loc String denoting location to fire at
+	 * @param grid1 Grid object for one player
+	 * @param grid2 Grid object for second player
+	 */
 	private void fireMissile(String loc, Grid grid1, Grid grid2) {
 		grid2.markOceanGrid(loc);
 		grid1.markTargetGrid(loc);
 	}
 	
+	/**
+	 * Simulates one turn for the player, firing a missile and informing them if a ship has been sunk
+	 * @param loc String denoting location to fire missile at
+	 */
 	private void playerTurn(String loc) {
 		fireMissile(loc, pGrid, cGrid);
+		if (isSunk(cGrid, loc))
+			System.out.println(pName + " has sunk one of" + cName + "'s ships");
 	}
 	
-	private void computerTurn(String loc) {
+	/**
+	 * Simulates one turn for the computer, firing a missile and informing them if a ship has been sunk
+	 */
+	private void computerTurn() { // Can make this the easy level later and hard will have more focused targeting
+		
+		// Generate a random location for the computer to shoot at
+		String loc;
+		Random rand = new Random(); 
+		int let = rand.nextInt(74) + 65;
+		int num = rand.nextInt(10) + 1;
+		loc = (char)(let) + Integer.toString(num);
+		
+		// Make sure the location can be shot at before firing missile
+		while (cGrid.alreadyShotMissile(loc)) {
+			let = rand.nextInt(74) + 65;
+			num = rand.nextInt(10) + 1;
+		}
+		
 		fireMissile(loc, cGrid, pGrid);
+
+		if (isSunk(pGrid, loc))
+			System.out.println(cName + " has sunk one of" + pName + "'s ships");
+	}
+	
+	/**
+	 * Helper method to help determine whether a ship has been sunk at the end of a turn
+	 * @param grid Grid which to check ships on
+	 * @param loc String denoting location where missile was shot
+	 * @return true if the ship at location has been sunk, false otherwise
+	 */
+	private boolean isSunk(Grid grid, String loc) {
+		boolean sunk = true;
+		Ship hitShip = grid.shipAtLocation(loc);
+		int hitShipIndex = grid.indexOfShip(hitShip);
+		for (int i = 0; i < grid.getShips().get(hitShipIndex).size(); i++)
+			if (!grid.getShips().get(hitShipIndex).get(i).equals("HIT")) { 
+				sunk = false;
+				i = grid.getShips().get(hitShipIndex).size(); // If any location on the ship has not been hit, the ship is not sunk so we can early exit loop
+			}
+		return sunk;
 	}
 }
