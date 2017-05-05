ba560a982a4bf7c81132b5128f09dd5342df8cc2
ktanakap <ktanakap@wellesley.edu>
2017-05-05 15:28:16 -0400

    Add files via upload

--- InstructionTab.java
+++ InstructionTab.java
@@ -23,44 +23,58 @@ public class InstructionTab extends JPanel {
     add(right);
 
     //JLabel for the game title
-    JLabel title = new JLabel("BATTLESHIP");
+    JLabel title = new JLabel("BATTLESHIP"); //insert picture? 
     title.setFont(new Font("Serif",Font.BOLD, 36));
 
     //JLabels for the left column 
-    //Object, contents, game instructions, salvo version
+    //Object of game, game "pieces", gameplay instructions, salvo version
     JLabel l1 = new JLabel("For 2 Players - Human vs Computer");
+    l1.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l2 = new JLabel("OJBECT OF THE GAME");
     l2.setFont(new Font("Serif",Font.BOLD, 26));
     JLabel l3 = new JLabel("Be the first to sink all 5 of your opponent's ships.");
+    l3.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l4 = new JLabel("CONTENTS");
     l4.setFont(new Font("Serif",Font.BOLD, 26));
     JLabel l5 = new JLabel("5 ships     white pegs     red pegs");
     JLabel l6 = new JLabel("     ocean grid     target grid");
+    l5.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l6.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l7 = new JLabel("PREPARE FOR BATTLE");
     l7.setFont(new Font("Serif",Font.BOLD, 26));
-    JLabel l8 = new JLabel("Each player must place all of their ships on their ocean grid");
+    JLabel l8 = new JLabel("Each player must place all of their ships on their ocean grid.");
     JLabel l9 = new JLabel("Rules for placing ships:");
     JLabel l10 = new JLabel("   -Ships may be placed horizontally or vertically, but not diagonally.");
     JLabel l11 = new JLabel("   -Ships are not allowed to overlap in letters, numbers, or the edge of the grid.");
     JLabel l12 = new JLabel("   -Position of the ships cannot be changed once the game has begun.");
+    l8.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l9.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l10.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l11.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l12.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l13 = new JLabel("HOW TO PLAY");
     l13.setFont(new Font("Serif",Font.BOLD, 26));
-    JLabel l14 = new JLabel("<html>The human player will begin first. Then, the human and computer will <br>alternate turns," + 
-                              " calling out one shot per turn to try and hit each other's ships.");
+    JLabel l14 = new JLabel("<html>The human player will begin first. Then, the human and computer will <br>" + 
+                              " alternate turns, calling out one shot per turn to try and hit each other's ships.");
     JLabel l15 = new JLabel("<html>To begin each turn, a player must pick a location on their target grid.<br>" +
                             "It will either be a HIT or MISS depending on whether the chosen location<br>" +
                             "is occupied by a ship on the opponent's ocean grid.<br>" +
                             "A HIT is recorded with a RED peg and a MISS is recorded with a WHITE peg.");
     JLabel l16 = new JLabel("Turns alternate with each shot called.");
+    l14.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l15.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l16.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l17 = new JLabel("SINKING A SHIP");
     l17.setFont(new Font("Serif",Font.BOLD, 26));
     JLabel l18 = new JLabel("<html>Once all the locations in any one ship are hit, it has been sunk. The name<br>" +
                             " of the sunken ship is announced and is marked separately at the top of the<br>" +
                             " game screen.");
+    l18.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l19 = new JLabel("WINNING THE GAME");
     l19.setFont(new Font("Serif",Font.BOLD, 26));
     JLabel l20 = new JLabel("<html>When a player sinks their opponent's entire fleet of five ships, they win <br>the" +
                             " game! The player then has the option to play again or quit.");
+    l20.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     JLabel l21 = new JLabel("HOW TO PLAY SALVO");
     l21.setFont(new Font("Serif",Font.BOLD, 26));
     JLabel l22 = new JLabel("<html>In Salvo mode, gameplay remains the same, but with on exception. Instead of <br>"+
@@ -68,7 +82,9 @@ public class InstructionTab extends JPanel {
                             "as the number of ships the opponent has remaining (i.e. the more ships sunk, <br>" +
                             "the fewer shots you get.");
     JLabel l23 = new JLabel("<html>A more challenging game can also be played where players do not disclose which <br>"+
-                            "ships are hit.");
+                            "ships are hit.<br><br>");
+    l22.setFont(new Font("Sans Serif",Font.PLAIN, 18));
+    l23.setFont(new Font("Sans Serif",Font.PLAIN, 18));
     
     //add JLabels to left panel
     left.add(title);
@@ -95,8 +111,10 @@ public class InstructionTab extends JPanel {
     left.add(l21);
     left.add(l22);
     left.add(l23);
+    left.add(new JButton("Let's Play")); //add play button at the bottom of the instruction page
     
     //JLabels for right column 
+    //visuals 
     JLabel r1 = new JLabel("FIGURE 1");
     JLabel r2 = new JLabel("FIGURE 2");
     JLabel r3 = new JLabel("FIGURE 3");
