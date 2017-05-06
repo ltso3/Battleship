3ade1040143529c9a74648c1cc1d5320d6bff29b
riannktang <rtang@wellesley.edu>
2017-05-06 14:16:14 -0400

    Riann

--- BattleshipGUI.java
+++ BattleshipGUI.java
@@ -1,10 +1,3 @@
-//********************************************************************
-// CS230 Final Project
-//
-// WelcomePage.java       CS230 Staff
-//
-//********************************************************************
-
 import javax.swing.*;
 
 public class BattleshipGUI {
@@ -19,15 +12,29 @@ public class BattleshipGUI {
     frame = new JFrame ("Welcome to Battleship!"); // Create JFrame for overall structure of GUI
     frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE); // Close GUI upon pressing x
     
-    wp = new WelcomePanel();
-    frame.getContentPane().add(wp);
+//	wp = new WelcomePanel();
+//	frame.getContentPane().add(wp);
+//	    
+	AddShipsPanel asp = new AddShipsPanel();
+	frame.getContentPane().add(asp);
+//	
+//	SetupPanel sp = new SetupPanel();
+//	frame.getContentPane().add(sp);
+//	
+//	InstructionTab it = new InstructionTab();
+//	frame.getContentPane().add(it);
+//	
+//	WinnerPopup wp = new WinnerPopup();
+//	frame.getContentPane().add(wp);
     
-//  AddShipsPanel asp = new AddShipsPanel();
-//  frame.getContentPane().add(asp);
-//  SetupPanel sp = new SetupPanel();
-//  frame.getContentPane().add(sp);
-//  InstructionTab it = new InstructionTab();
-//  frame.getContentPane().add(it);
+//	LoserPopup lp = new LoserPopup();
+//	frame.getContentPane().add(lp);
+    
+//	HitPopup hp = new HitPopup();
+//	frame.getContentPane().add(hp);
+    
+//	MissPopup mp = new MissPopup();
+//	frame.getContentPane().add(mp);
     
     frame.pack();
     frame.setVisible(true);
