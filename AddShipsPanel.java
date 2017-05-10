216ce484a9b45b416d68f785f4e5eec84d570736
ltso3 <ltso@wellesley.edu>
2017-05-10 19:10:36 -0400

    Add files via upload

--- AddShipsPanel.java
+++ AddShipsPanel.java
@@ -4,10 +4,12 @@ import javax.swing.*;
 
 public class AddShipsPanel extends JPanel {
   
-  private JPanel upperText1, upperText2, upperText3, add, done, lowerText;
-  private JComboBox shipCombo, orientationCombo;
-  private TextField startCoord;
-  private JButton addBtn, doneBtn;
+  private JPanel upperText1, upperText2, upperText3, add, done, lowerText,
+                 destroyer, sub, cruiser, battleship, carrier;
+  private JComboBox shipCombo, orientationCombo, shipCombo1, orientationCombo1, shipCombo2, orientationCombo2, 
+                    shipCombo3, orientationCombo3, shipCombo4, orientationCombo4;
+  private TextField startCoord, startCoord1, startCoord2, startCoord3, startCoord4;
+  private JButton doneBtn;
   private JLabel l1, l2, l3, l4, newShip;
   private PlayBattleship game;
   private BattleshipGUI b;
@@ -33,13 +35,14 @@ public class AddShipsPanel extends JPanel {
     upperText2.setBackground(new Color(197, 204, 208));
     upperText3.setBackground(new Color(197, 204, 208));
     add.setBackground(new Color(197, 204, 208));
+    add.setLayout(new BoxLayout(add, BoxLayout.Y_AXIS));
     done.setBackground(new Color(197, 204, 208));
     lowerText.setBackground(new Color(197, 204, 208));
     
     // Set  text for each panel
     l1 = new JLabel("To begin, input a starting coordinate and select an orientation for each ship, then click [Add]."); 
-    l2 = new JLabel("'Vertical' shipLocs go from the given location down and 'horizontal' shipLocs go from the given location right.");
-    l3 = new JLabel("Once you've entered all 5 shipLocs, press [Let's Play!]"); 
+    l2 = new JLabel("'Vertical' ships go from the given location down and 'horizontal' ships go from the given location right.");
+    l3 = new JLabel("Once you've entered all 5 ships, press [Let's Play!]"); 
     l1.setFont(new Font("Russo One", Font.PLAIN, 14));
     l2.setFont(new Font("Russo One", Font.PLAIN, 14));
     l3.setFont(new Font("Russo One", Font.PLAIN, 14));
@@ -47,32 +50,101 @@ public class AddShipsPanel extends JPanel {
     upperText2.add(l2);
     upperText3.add(l3);
 
-    l4 = new JLabel("Information on added shipLocs will appear here.");
+    l4 = new JLabel("");
     lowerText.add(l4);
     newShip = new JLabel("");
     
+    destroyer = new JPanel();
+    sub = new JPanel();
+    cruiser = new JPanel();
+    battleship = new JPanel();
+    carrier = new JPanel();
+    destroyer.setBackground(new Color(197, 204, 208));
+    sub.setBackground(new Color(197, 204, 208));
+    cruiser.setBackground(new Color(197, 204, 208));
+    battleship.setBackground(new Color(197, 204, 208));
+    carrier.setBackground(new Color(197, 204, 208));
+    
+    // Destoyer input line
     // Create combo box for ship type
-    String[] shipTs  = {"...", "Destroyer", "Submarine", "Cruiser", "Battleship", "Carrier"};
-    shipCombo = new JComboBox(shipTs);
-    add.add(new Label("Ship type: "));
-    add.add(shipCombo);
+    destroyer.add(new Label("Destroyer (Length 2) | "));
     
     // Create combo box for ship orientation and add to all ship JPanels
     String[] orientation  = {"...", "Horizontal", "Vertical"};
     orientationCombo = new JComboBox(orientation);
-    add.add(new Label("Orientation: "));
-    add.add(orientationCombo);
+    destroyer.add(new Label("Orientation: "));
+    destroyer.add(orientationCombo);
     
     // Create text field to input starting location for each ship
     startCoord = new TextField(3); // length 3
-    startCoord.addActionListener(new AddShipsListener());
-    add.add(new Label("Starting coordinate: "));
-    add.add(startCoord);
+    destroyer.add(new Label("Starting coordinate: "));
+    destroyer.add(startCoord);
     
-    // Create add buttons for each ship
-    addBtn = new JButton("Add");
-    addBtn.addActionListener(new AddShipsListener());
-    add.add(addBtn);
+    add.add(destroyer);
+    
+    // Submarine input line
+    sub.add(new Label("Submarine (Length 3) | "));
+    
+    // Create combo box for ship orientation and add to all ship JPanels
+    String[] orientation1  = {"...", "Horizontal", "Vertical"};
+    orientationCombo1 = new JComboBox(orientation1);
+    sub.add(new Label("Orientation: "));
+    sub.add(orientationCombo1);
+    
+    // Create text field to input starting location for each ship
+    startCoord1 = new TextField(3); // length 3
+    sub.add(new Label("Starting coordinate: "));
+    sub.add(startCoord1);
+    
+    add.add(sub);
+    
+    // Cruiser input line
+    cruiser.add(new Label("Cruiser (Length 3) | "));
+    
+    // Create combo box for ship orientation and add to all ship JPanels
+    String[] orientation2  = {"...", "Horizontal", "Vertical"};
+    orientationCombo2 = new JComboBox(orientation2);
+    cruiser.add(new Label("Orientation: "));
+    cruiser.add(orientationCombo2);
+    
+    // Create text field to input starting location for each ship
+    startCoord2 = new TextField(3); // length 3
+    cruiser.add(new Label("Starting coordinate: "));
+    cruiser.add(startCoord2);
+    
+    add.add(cruiser);
+    
+    // Battleship input line
+    battleship.add(new Label("Battleship (Length 4) | "));
+    
+    // Create combo box for ship orientation and add to all ship JPanels
+    String[] orientation3  = {"...", "Horizontal", "Vertical"};
+    orientationCombo3 = new JComboBox(orientation3);
+    battleship.add(new Label("Orientation: "));
+    battleship.add(orientationCombo3);
+    
+    // Create text field to input starting location for each ship
+    startCoord3 = new TextField(3); // length 3
+    battleship.add(new Label("Starting coordinate: "));
+    battleship.add(startCoord3);
+    
+    add.add(battleship);
+    
+    // Carrier input line
+    carrier.add(new Label("Carrier (Length 5) | "));
+    
+    // Create combo box for ship orientation and add to all ship JPanels
+    String[] orientation4  = {"...", "Horizontal", "Vertical"};
+    orientationCombo4 = new JComboBox(orientation4);
+    carrier.add(new Label("Orientation: "));
+    carrier.add(orientationCombo4);
+    
+    // Create text field to input starting location for each ship
+    startCoord4 = new TextField(3); // length 3
+    carrier.add(new Label("Starting coordinate: "));
+    carrier.add(startCoord4);
+    
+    add.add(carrier);
     
     // Create done button
     doneBtn = new JButton("Let's Play!");
@@ -92,26 +164,47 @@ public class AddShipsPanel extends JPanel {
     add(lowerText);
   }
   
-  // Need method to ensure only one of each type of ship is added
-  // Also need to update grid with different color buttons, or a ship picture. Picture would need to be split into many pieces and resized.
-  private class AddShipsListener implements ActionListener {
+  // Need way to ensure play button only works if the ships have already been added
+  private class DoneListener implements ActionListener {
     public void actionPerformed(ActionEvent event) {
-      String sType, sCoord, orientation;
-      
+      String sType, sCoord, orientation, sType1, sCoord1, orientation1, sType2, sCoord2, orientation2,
+             sType3, sCoord3, orientation3, sType4, sCoord4, orientation4;
+        
       // Gets values user has selected
-      sType = shipCombo.getSelectedItem().toString();
       orientation = orientationCombo.getSelectedItem().toString();
       sCoord = startCoord.getText(); // Need to check that starting coordinate is valid
       
-      l4.setText("");
+      orientation1 = orientationCombo1.getSelectedItem().toString();
+      sCoord1 = startCoord1.getText(); // Need to check that starting coordinate is valid
+      
+      orientation2 = orientationCombo2.getSelectedItem().toString();
+      sCoord2 = startCoord2.getText(); // Need to check that starting coordinate is valid
+      
+      orientation3 = orientationCombo3.getSelectedItem().toString();
+      sCoord3 = startCoord3.getText(); // Need to check that starting coordinate is valid
+      
+      orientation4 = orientationCombo4.getSelectedItem().toString();
+      sCoord4 = startCoord4.getText(); // Need to check that starting coordinate is valid
+      
       
       // Make sure that user has selected  a value for everything
-      if (!sType.equals("...") && !orientation.equals("...") && !sCoord.equals("") ) {
-        Ship temp = new Ship(sType, orientation, sCoord);
-        if(game.getPGrid().isValidStart(temp)) {
-          game.getPGrid().addShip(temp);
+      if (!orientation.equals("...") && !sCoord.equals("") && !orientation1.equals("...") && !sCoord1.equals("")
+         && !orientation2.equals("...") && !sCoord2.equals("") && !orientation3.equals("...") && !sCoord3.equals("")
+         && !orientation4.equals("...") && !sCoord4.equals("")) {
+        Ship destroyer = new Ship("Destroyer", orientation, sCoord);
+        Ship submarine = new Ship("Submarine", orientation1, sCoord1);
+        Ship cruiser = new Ship("Cruiser", orientation2, sCoord2);
+        Ship battleship = new Ship("Battleship", orientation3, sCoord3);
+        Ship carrier = new Ship("Carrier", orientation4, sCoord4);
+        if(game.getPGrid().isValidStart(destroyer) && game.getPGrid().isValidStart(submarine)
+          && game.getPGrid().isValidStart(cruiser) && game.getPGrid().isValidStart(battleship) 
+          && game.getPGrid().isValidStart(carrier)) {
+          game.getPGrid().addShip(destroyer);
+          game.getPGrid().addShip(submarine);
+          game.getPGrid().addShip(cruiser);
+          game.getPGrid().addShip(battleship);
+          game.getPGrid().addShip(carrier);
           l4.setText("");
-          newShip = new JLabel(temp.toString());
           lowerText.add(newShip, 0);
         }
         else 
@@ -120,12 +213,6 @@ public class AddShipsPanel extends JPanel {
       else {
         l4.setText("Please make sure that you have typed in a coordinate and selected values for all the dropdown boxes");
       }
-    }
-  }
-  
-  // Need way to ensure play button only works if the ships have already been added
-  private class DoneListener implements ActionListener {
-    public void actionPerformed(ActionEvent event) {
       boolean allShipsAdded = true;
       for(int i = 0; i < game.getPGrid().getShipLocs().size(); i++) {
         if(game.getPGrid().getShipLocs().get(i).size() == 0) 
@@ -134,12 +221,12 @@ public class AddShipsPanel extends JPanel {
       if(allShipsAdded) {
         b.frame.getContentPane().removeAll();
         b.asp = null;
-        b.frame.getContentPane().add(new SetupPanel(game, b));
+        b.frame.getContentPane().add(new PlayPanel(game, b));
         b.frame.setVisible(true);
       }
       
       else {
-        l4.setText("Please make sure that you have added all shipLocs");
+        l4.setText("Please make sure that you have added all ships");
       }
     }
   }
---
+++ PlayPanel.java
@@ -0,0 +1,178 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PlayPanel extends JPanel {
+  private JPanel shipLocs, grids, gridsLP, shipLocsLP, fire, tgp, ogp;
+  private TextField fireCoord;
+  private JButton help, fireBtn;
+  private JLabel gridsL, shipLocsL;
+  protected PlayBattleship game;
+  private BattleshipGUI b;
+  
+  private JButton[][] tgpButtons;
+  private JButton[][] ogpButtons;
+  
+  public PlayPanel(PlayBattleship game, BattleshipGUI b) {
+    this.game = game;
+    this.b = b;
+    
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    setBackground(new Color(92, 135, 149));
+    
+    gridsLP = new JPanel();
+    gridsLP.setBackground(new Color(92, 135, 149));
+    gridsL = new JLabel("Target Grid");
+    gridsL.setBackground(new Color(92, 135, 149));
+    gridsL.setFont(new Font("Russo One",Font.PLAIN, 25));
+    gridsL.setForeground(Color.white);
+    gridsLP.add(gridsL);
+    add(gridsLP);
+    
+    // ---------------------------------------------------------------------------//
+    tgp = new JPanel();
+    grids = new JPanel();
+    grids.setLayout(new BoxLayout(grids, BoxLayout.Y_AXIS));
+    
+    tgp.setLayout(new GridLayout(11, 11, 0, 0)); 
+    tgp.setBackground(new Color(92, 135, 149));
+    
+    tgpButtons = new JButton[12][12];
+    String[] letters = {"A","B","C","D","E","F","G","H","I","J"};
+    
+    for(int i = 0; i < 11; i++) {
+      for(int j = 0; j < 11; j++) {
+        if(i == 0 && j != 0) 
+          tgpButtons[i][j] = new JButton(letters[j - 1]);
+        else if(j == 0 && i != 0)
+          tgpButtons[i][j] = new JButton(Integer.toString(i));
+        else {
+          tgpButtons[i][j] = new JButton("");
+        }
+        
+        tgpButtons[i][j].setBackground(new Color(197, 204, 208));
+        tgpButtons[i][j].setOpaque(true);
+        tgpButtons[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
+        tgpButtons[i][j].setPreferredSize(new Dimension(110, 50));
+        tgp.add(tgpButtons[i][j]);
+      }
+    }
+    
+    grids.add(tgp);
+    
+    //----------------------------------------------------------------------------------------//
+    
+    grids.add(Box.createRigidArea(new Dimension(0,30)));
+    
+    fire = new JPanel();
+    fireCoord = new TextField(3); // length 3
+    fire.add(new Label("Fire on: "));
+    fire.add(fireCoord);
+    
+    fireBtn = new JButton("Fire!");
+    fire.add(fireBtn);
+    fireBtn.setBackground(new Color(94, 197, 110));
+    fireBtn.setOpaque(true);
+    fireBtn.setBorder(BorderFactory.createLineBorder(Color.black));
+    fireBtn.setPreferredSize(new Dimension(40, 25));
+    fireBtn.addActionListener(new FireButtonListener());
+    
+    fire.setBackground(new Color(92, 135, 149));
+    grids.add(fire);
+    
+    grids.add(Box.createRigidArea(new Dimension(0,30)));
+    
+    ogp = new JPanel();
+    ogp.setLayout(new GridLayout(11, 11, 0, 0)); 
+    ogp.setBackground(new Color(92, 135, 149));
+    
+    ogpButtons = new JButton[12][12];
+    
+    for(int l = 0; l < 11; l++) {
+      for(int m = 0; m < 11; m++) {
+        if(l == 0 && m != 0) 
+          ogpButtons[l][m] = new JButton(letters[m - 1]);
+        else if(m == 0 && l != 0)
+          ogpButtons[l][m] = new JButton(Integer.toString(l));
+        else {
+          ogpButtons[l][m] = new JButton("");
+        }
+        
+        ogpButtons[l][m].setBackground(new Color(197, 204, 208));
+        ogpButtons[l][m].setOpaque(true);
+        ogpButtons[l][m].setBorder(BorderFactory.createLineBorder(Color.black));
+        ogpButtons[l][m].setPreferredSize(new Dimension(110, 50));
+        ogp.add(ogpButtons[l][m]);
+      }
+    }
+    
+    grids.add(ogp); 
+    
+    for(LinkedList<String> locs : game.getPGrid().getShipLocs()) {
+      for(String loc : locs) {
+        int row = Integer.parseInt(loc.substring(1));
+        int column = (int) loc.charAt(0) - 64;
+        ogpButtons[row][column].setBackground(new Color(96, 96, 96));
+      }
+    }
+    
+    add(grids, BorderLayout.CENTER);
+    grids.setBackground(new Color(92, 135, 149));
+    add(grids);
+    
+    shipLocsLP = new JPanel();
+    shipLocsLP.setBackground(new Color(92, 135, 149));
+    shipLocsL = new JLabel("Ocean Grid");
+    shipLocsL.setBackground(new Color(92, 135, 149));
+    shipLocsL.setFont(new Font("Russo One",Font.PLAIN, 25));
+    shipLocsL.setForeground(Color.white);
+    shipLocsLP.add(shipLocsL);
+    add(shipLocsLP);
+    
+    shipLocs = new JPanel();
+    help = new JButton(new ImageIcon("help-icon.png"));
+    help.setPreferredSize(new Dimension(35,35));
+    help.addActionListener(new ButtonListener());
+    help.setBackground(new Color(92, 135, 149));
+    shipLocs.setBackground(new Color(92, 135, 149));
+    shipLocs.add(help);
+    add(shipLocs);
+  }
+  
+  private class ButtonListener implements ActionListener {
+    public void actionPerformed (ActionEvent event) {
+      JFrame frame = new JFrame ("BATTLESHIP"); // Create JFrame for overall structure of GUI
+      frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE); // Close GUI upon pressing x
+      
+      InstructionTab it = new InstructionTab();
+      frame.getContentPane().add(it);
+      
+      frame.pack();
+      frame.setVisible(true);
+    }
+  }
+  
+  private class FireButtonListener implements ActionListener {
+    public void actionPerformed (ActionEvent event) { 
+      String loc = fireCoord.getText();
+      game.playerTurn(loc);
+      int row = Integer.parseInt(loc.substring(1));
+      int column = (int) loc.charAt(0) - 64;
+      LinkedList<Integer> temp = new LinkedList<Integer>();
+      temp.add(row);
+      temp.add(column);
+      
+      if(game.fireMissile(loc, game.getPGrid(), game.getCGrid()))
+        tgpButtons[row][column].setIcon(new ImageIcon("red.png"));
+      else 
+        tgpButtons[row][column].setIcon(new ImageIcon("white.png"));
+      
+      //invalidate();
+//      repaint();
+      tgp.revalidate();
+    }
+  }
+}
+
+
