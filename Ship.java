0495d0caf8a29fa3c0881235795d43ddf3fa7d73
ltso3 <ltso@wellesley.edu>
2017-05-11 12:09:00 -0400

    Add files via upload

--- Ship.java
+++ Ship.java
@@ -1,36 +1,45 @@
 public class Ship {
   
+  // Initialize private instance variables
   private String name;
   private String orientation;
   private int length;
   private String startCoord;
   
   public Ship(String name, String orientation, String startCoord) {  
+    // Assign private instance variables
     this.name = name;
     this.orientation = orientation;
     this.length = setLength(name);
     this.startCoord = startCoord;
   }
   
+  /* Getter method to retrieve name */
   public String getName() {
     return name;
   }
   
+  /* Getter method to retrieve orientation */
   public String getOrientation() {
     return this.orientation;
   }
   
+  /* Getter method to retrieve start coordinate */
   public String getStartCoord() {
     return this.startCoord;
   }
   
+  /* Setter method to set orientation */
   public void setOrientation(String o) {
-	  orientation = o;
-  }
-  public void setStartCoord(String newCoord) {
-	  startCoord = newCoord;
+   orientation = o;
   }
   
+  /* Setter method to set start coordinate */
+  public void setStartCoord(String newCoord) {
+   startCoord = newCoord;
+  }
+  
+  /* Setter method to set length according to ship name */
   public int setLength(String name) {
     int len = 0;
     if(name.equals("Carrier"))
@@ -48,14 +57,16 @@ public class Ship {
     return len;
   }
   
+  /* Getter method to retrieve length */
   public int getLength() {
     return this.length;
   }
   
+  /* Format Ship in a String with the appropriate values */
   public String toString() {
-	  String s = "";
-	  s += "Ship: " +name + ", Length: " + length + " Orientation: " + orientation + " Starting coordinate: " + startCoord + "\n";
-	  return s;
+   String s = "";
+   s += "Ship: " +name + ", Length: " + length + " Orientation: " + orientation + " Starting coordinate: " + startCoord + "\n";
+   return s;
   }
   
   public static void main(String[] args) {
